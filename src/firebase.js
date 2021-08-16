0390303d38821760ec315d3745af2133dc757ad2
unknown <ishworineupane135@gmail.com>
2021-08-16 12:04:43 +0545

    Add existing file

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBiSgQXdJuW683ZrjsRbnOoHnGpxWyuM9c",
+  authDomain: "yt-21f68.firebaseapp.com",
+  projectId: "yt-21f68",
+  storageBucket: "yt-21f68.appspot.com",
+  messagingSenderId: "874947853492",
+  appId: "1:874947853492:web:c5de84c4fa656665bcabe1",
+  measurementId: "G-ZHZ07EEWX9"
+};
